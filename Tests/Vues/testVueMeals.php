d0e040004d6b9e5b49387deabb25171b5ffdd267
Prabhjot Singh Josan <prabhjosan16@gmail.com>
2020-05-02 15:35:19 -0400

    erreur test

--- Tests/Vues/testVueMeals.php
+++ Tests/Vues/testVueMeals.php
@@ -3,17 +3,14 @@
 require_once 'Vue/Vue.php';
 $meals = [
 
-	
+	[
         'Meal_ID' => '18',
 		'Other_Details' => 'Dessert',
         'Meal_Details' => 'Gateau à la vanille',
         'Date_of_meal' => '2020-03-06 13:56:05',
 		'Cost_of_meal' => 15'
-	
+	]
 
 ];
 $vue = new Vue('Meals');
 $vue->generer(['meals' => $meals]);
-
-
-
